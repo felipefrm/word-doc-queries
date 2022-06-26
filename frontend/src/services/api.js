73acc6cfdebf23125242715b8777e79fe5a6a3fb
Felipe Melo <narulee@hotmail.com>
2022-06-26 15:58:59 -0300

    logout user when api returns 401

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,4 +4,14 @@ const api = axios.create({
   baseURL: 'http://localhost:3086',
 });
 
+api.interceptors.response.use(response => {
+  return response;
+}, error => {
+ if (error.response.status === 401) {
+  localStorage.clear()
+  window.location.reload()
+ }
+ return error;
+});
+
 export default api;
