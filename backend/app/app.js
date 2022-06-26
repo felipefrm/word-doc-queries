25955c04f7b3984d7a3eb8ca0797b632e8e0f3aa
Felipe Melo <narulee@hotmail.com>
2022-06-26 14:28:56 -0300

    added cors

--- backend/app/app.js
+++ backend/app/app.js
@@ -3,9 +3,12 @@ const baseRoutes = require('./routes');
 const userControllerRoutes = require('./controllers/user.controller');
 const documentControllerRoutes = require('./controllers/document.controller');
 const bodyParser = require('body-parser')
+const cors = require('cors')
+
 class App {
     constructor() {
         this.server = express();
+        this.server.use(cors());
         this.server.use(express.urlencoded({ extended: false }));
         this.server.use(bodyParser.json());
         this.initRoutes();
