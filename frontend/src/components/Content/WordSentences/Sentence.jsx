e2da5008647f0045fd990f50aeff79d8b9bce1d3
Felipe Melo <narulee@hotmail.com>
2022-06-27 20:46:04 -0300

    case insensitive bold match word

--- frontend/src/components/Content/WordSentences/Sentence.jsx
+++ frontend/src/components/Content/WordSentences/Sentence.jsx
@@ -2,9 +2,9 @@ import { Text } from "@chakra-ui/react";
 
 export function Sentence({ word, index, children }) {
   
-  const wordRegex = new RegExp(word, 'g');
-  const sentence = children.replace(wordRegex, '<b>' + word + '</b>')
-  console.log(sentence)
+  const wordRegex = new RegExp("\\b" + word + "\\b", 'gi')
+  const sentence = children.replace(wordRegex, (w) => '<b>' + w + '</b>');
+
   
   return (
     <Text color="#3A5163" fontSize="xl">
